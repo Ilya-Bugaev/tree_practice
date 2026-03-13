b02595211e60d9658cc89eb5a211601697776fde
stuffacc <st141867@spbu.ru>
2026-03-13 21:52:35 +0300

    webkitt

--- src/bst.h
+++ src/bst.h
@@ -3,20 +3,20 @@
 
 typedef struct Node {
     int value;
-    struct Node *leftChild;
-    struct Node *rightChild;
+    struct Node* leftChild;
+    struct Node* rightChild;
 } Node;
 
 typedef struct {
     Node* root;
 } BST;
 
-typedef enum Direction {LEFT, RIGHT} Direction;
+typedef enum Direction { LEFT,
+    RIGHT } Direction;
 
 void bstInsert(BST* tree, int value);
 
 bool bstContains(BST* tree, int value);
 
 void bstFree(BST* tree);
-
 void bstFreeNode(Node* node);
