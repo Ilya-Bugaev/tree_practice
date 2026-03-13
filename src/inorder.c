01f3881679940cbda8d7943a37826f19a7340b17
Ilya Bugaev <bugaevi07@mail.ru>
2026-03-14 01:42:29 +0300

    Update inorder.c

--- src/inorder.c
+++ src/inorder.c
@@ -1,60 +1,68 @@
 #include "inorder.h"
+
 #include "bst.h"
+
 #include <stdio.h>
 
-void recInorder(Node *node) {
-  if (node == NULL) {
-    return;
-  }
+void recInorder(Node* node)
+{
+    if (node == NULL) {
+        return;
+    }
 
-  recInorder(node->leftChild);
-  printf("%d ", node->value);
-  recInorder(node->rightChild);
+    recInorder(node->leftChild);
+    printf("%d ", node->value);
+    recInorder(node->rightChild);
 }
 
-void recPreorder(Node *node) {
-  if (node == NULL) {
-    return;
-  }
+void recPreorder(Node* node)
+{
+    if (node == NULL) {
+        return;
+    }
 
-  printf("%d ", node->value);
-  recPreorder(node->leftChild);
-  recPreorder(node->rightChild);
+    printf("%d ", node->value);
+    recPreorder(node->leftChild);
+    recPreorder(node->rightChild);
 }
 
-void recPostorder(Node *node) {
-  if (node == NULL) {
-    return;
-  }
+void recPostorder(Node* node)
+{
+    if (node == NULL) {
+        return;
+    }
 
-  recPostorder(node->leftChild);
-  recPostorder(node->rightChild);
-  printf("%d ", node->value);
+    recPostorder(node->leftChild);
+    recPostorder(node->rightChild);
+    printf("%d ", node->value);
 }
 
-void bstInorder(BST *tree) {
-  if (tree == NULL) {
-    return;
-  }
+void bstInorder(BST* tree)
+{
+    if (tree == NULL) {
+        return;
+    }
 
-  recInorder(tree->root);
-  printf("\n");
+    recInorder(tree->root);
+    printf("\n");
 }
 
-void bstPreorder(BST *tree) {
-  if (tree == NULL) {
-    return;
-  }
+void bstPreorder(BST* tree)
+{
+    if (tree == NULL) {
+        return;
+    }
 
-  recPreorder(tree->root);
-  printf("\n");
+    recPreorder(tree->root);
+    printf("\n");
 }
 
-void bstPostorder(BST *tree) {
-  if (tree == NULL) {
-    return;
-  }
+void bstPostorder(BST* tree)
+{
+    if (tree == NULL) {
+        return;
+    }
 
-  recPostorder(tree->root);
-  printf("\n");
+    recPostorder(tree->root);
+    printf("\n");
 }
