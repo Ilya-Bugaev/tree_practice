9fc1900c30eeec3d022691c7e6f16e50f73dd4de
stuffacc <st141867@spbu.ru>
2026-03-16 20:15:33 +0300

    task f bstIsValid

---
+++ src/checkBst.c
@@ -0,0 +1,35 @@
+#include "checkBst.h"
+#include "iterator.h"
+
+
+bool bstIsValid(BST* tree) {
+    if ((tree == 0) || (tree->root == 0)) {
+        return true;
+    }
+
+    Iterator* iterator = iteratorInit(tree);
+
+    int prevValue = iteratorNext(iterator);
+    int currValue = 0;
+
+    if (!iteratorHasNext(iterator)) {
+        iteratorFree(&iterator);
+        return true;
+    }
+    
+
+    while (iteratorHasNext(iterator)) {
+        currValue = iteratorNext(iterator);
+
+        if (currValue <= prevValue) {
+            iteratorFree(&iterator);
+            return false;
+        }
+        
+        prevValue = currValue;
+    }
+
+    iteratorFree(&iterator);
+    return true;
+    
+}
--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "bst.h"
 #include "iterator.h"
+#include "checkBst.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -34,5 +35,45 @@ void main()
     printf("%d\n", iteratorNext(iterator));
 
     iteratorFree(&iterator);
+
+    printf("BST IS VALID: %d\n", bstIsValid(tree));
+    tree->root->leftChild->leftChild->rightChild->leftChild->rightChild->value = 1000;
+    iterator = iteratorInit(tree);
+
+    while (iteratorHasNext(iterator)) {
+        printf("%d ", iteratorNext(iterator));
+    }
+    printf("\n");
+
+    iteratorFree(&iterator);
+
+    printf("BST IS VALID: %d\n", bstIsValid(tree));
+
+    BST* tree2 = malloc(sizeof(BST));
+    tree2->root = malloc(sizeof(Node));
+    tree2->root->value = 100;
+
+    tree2->root->leftChild = malloc(sizeof(Node));
+    tree2->root->leftChild->value = 99;
+
+    printf("BST2 IS VALID: %d\n", bstIsValid(tree2));
+
+    tree2->root->leftChild->value = 100;
+
+    printf("BST2 IS VALID: %d\n", bstIsValid(tree2));
+
+    tree2->root->rightChild = malloc(sizeof(Node));
+    tree2->root->leftChild->value = 99;
+    tree2->root->rightChild->value = 99;
+
+    printf("BST2 IS VALID: %d\n", bstIsValid(tree2));
+
+
+    tree2->root->rightChild->value = 999;
+
+    printf("BST2 IS VALID: %d\n", bstIsValid(tree2));
+
     bstFree(tree);
+    bstFree(tree2);
+
 }
