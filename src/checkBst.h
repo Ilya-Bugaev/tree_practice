dfe2105f208effdadbcfb6c55db0a6f85ea510d1
stuffacc <st141867@student.spbu.ru>
2026-03-21 15:47:57 +0300

    Iterator (#5)
    * Update src/bst.h
    
    Co-authored-by: NIKITAGOLOVKIN <golovkinnv7@gmail.com>
    
    * Update src/bst.h
    
    Co-authored-by: NIKITAGOLOVKIN <golovkinnv7@gmail.com>
    
    * Iterator
    
    * iterator main
    
    * Webkit
    
    * clear comments
    
    * review changes
    
    * Task f bst is valid (#7)
    
    * task f bstIsValid
    
    * webkit
    
    * review changes
    
    * task g (#6)
    
    * task g
    
    * webkit
    
    * review changes
    
    Co-authored-by: Ilya Bugaev <bugaevi07@mail.ru>
    Co-authored-by: NIKITAGOLOVKIN <golovkinnv7@gmail.com>

---
+++ src/checkBst.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "bst.h"
+#include <stdbool.h>
+
+bool bstIsValid(BST* tree);
--- src/main.c
+++ src/main.c
@@ -1,4 +1,8 @@
 #include "bst.h"
+#include "checkBst.h"
+#include "iterator.h"
+#include "statistic.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -8,14 +12,80 @@ void main()
 
     tree->root = NULL;
 
-    bstInsert(tree, 6);
+    bstInsert(tree, 100);
+    bstInsert(tree, 60);
+    bstInsert(tree, 50);
+    bstInsert(tree, 30);
+    bstInsert(tree, 57);
+    bstInsert(tree, 55);
+    bstInsert(tree, 52);
+    bstInsert(tree, 56);
+    bstInsert(tree, 54);
+    bstInsert(tree, 53);
+    bstInsert(tree, 200);
+    bstInsert(tree, 10);
+    bstInsert(tree, 9);
+    bstInsert(tree, 1);
 
-    bstInsert(tree, 4);
-    bstInsert(tree, 5);
+    Iterator* iterator = iteratorInit(tree);
 
-    printf("%d\n", tree->root->value);
-    printf("%d\n", tree->root->leftChild->value);
-    printf("%d\n", tree->root->leftChild->rightChild->value);
+    while (iteratorHasNext(iterator)) {
+        printf("%d ", iteratorNext(iterator));
+    }
+    printf("\n");
+    printf("%d\n", iteratorNext(iterator));
+
+    iteratorFree(&iterator);
+
+    int k = 0;
+    printf("K: %d MIN:%d\n", k, bstKthMin(tree, k));
+
+    k = 5;
+    printf("K: %d MIN:%d\n", k, bstKthMin(tree, k));
+
+    k = 14;
+    printf("K: %d MIN:%d\n", k, bstKthMin(tree, k));
+
+    k = 15;
+    printf("K: %d MIN:%d\n", k, bstKthMin(tree, k));
+
+    printf("EMPTY: K: %d MIN:%d\n", k, bstKthMin(tree, k));
+    printf("BST IS VALID: %d\n", bstIsValid(tree));
+    tree->root->leftChild->leftChild->rightChild->leftChild->rightChild->value = 1000;
+    iterator = iteratorInit(tree);
+
+    while (iteratorHasNext(iterator)) {
+        printf("%d ", iteratorNext(iterator));
+    }
+    printf("\n");
+
+    iteratorFree(&iterator);
+
+    printf("BST IS VALID: %d\n", bstIsValid(tree));
+
+    BST* tree2 = malloc(sizeof(BST));
+    tree2->root = malloc(sizeof(Node));
+    tree2->root->value = 100;
+
+    tree2->root->leftChild = malloc(sizeof(Node));
+    tree2->root->leftChild->value = 99;
+
+    printf("BST2 IS VALID: %d\n", bstIsValid(tree2));
+
+    tree2->root->leftChild->value = 100;
+
+    printf("BST2 IS VALID: %d\n", bstIsValid(tree2));
+
+    tree2->root->rightChild = malloc(sizeof(Node));
+    tree2->root->leftChild->value = 99;
+    tree2->root->rightChild->value = 99;
+
+    printf("BST2 IS VALID: %d\n", bstIsValid(tree2));
+
+    tree2->root->rightChild->value = 999;
+
+    printf("BST2 IS VALID: %d\n", bstIsValid(tree2));
 
     bstFree(tree);
+    bstFree(tree2);
 }
